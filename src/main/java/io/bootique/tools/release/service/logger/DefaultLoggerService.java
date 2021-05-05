86cde0172fca3dc671f502cf708f94551f64ab07
Nikita Timofeev <stariy95@gmail.com>
2021-05-05 16:13:10 +0300

    Fix custom logger

--- src/main/java/io/bootique/tools/release/service/logger/DefaultLoggerService.java
+++ src/main/java/io/bootique/tools/release/service/logger/DefaultLoggerService.java
@@ -2,6 +2,7 @@ package io.bootique.tools.release.service.logger;
 
 import ch.qos.logback.classic.Logger;
 import io.bootique.tools.release.model.release.ReleaseDescriptor;
+import io.bootique.tools.release.service.desktop.DesktopService;
 import io.bootique.tools.release.service.preferences.PreferenceService;
 import org.slf4j.LoggerFactory;
 
@@ -10,7 +11,7 @@ import javax.inject.Inject;
 
 public class DefaultLoggerService implements LoggerService {
 
-    private static final Logger ROOT_LOGGER = (Logger) LoggerFactory.getLogger(Logger.ROOT_LOGGER_NAME);
+    private static final Logger LOGGER = (Logger) LoggerFactory.getLogger(DesktopService.class);
 
     @Inject
     PreferenceService preferenceService;
@@ -20,7 +21,7 @@ public class DefaultLoggerService implements LoggerService {
 
     @Override
     public void prepareLogger(ReleaseDescriptor releaseDescriptor) {
-        multiAppender = (MultiAppender) ROOT_LOGGER.getAppender("multiAppender");
+        multiAppender = (MultiAppender) LOGGER.getAppender("multiAppender");
         multiAppender.createAppenderMap(releaseDescriptor,  preferenceService.get(LoggerService.LOGGER_BASE_PATH));
         this.releaseVersion = releaseDescriptor.getReleaseVersion();
     }
