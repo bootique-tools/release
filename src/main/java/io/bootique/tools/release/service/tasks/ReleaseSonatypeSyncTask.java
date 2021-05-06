0df677873668f9738efb6a149fa1f11ad050de5e
Nikita Timofeev <stariy95@gmail.com>
2021-05-06 12:17:09 +0300

    Automatically deal with a remote staging repository

--- src/main/java/io/bootique/tools/release/service/tasks/ReleaseSonatypeSyncTask.java
+++ src/main/java/io/bootique/tools/release/service/tasks/ReleaseSonatypeSyncTask.java
@@ -1,8 +1,9 @@
 package io.bootique.tools.release.service.tasks;
 
 import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.util.List;
 import java.util.function.Function;
+import java.util.stream.Collectors;
 
 import javax.inject.Inject;
 
@@ -18,8 +19,6 @@ import io.bootique.tools.release.service.preferences.PreferenceService;
 import io.bootique.tools.release.service.release.ReleaseService;
 
 public class ReleaseSonatypeSyncTask implements Function<Repository, String> {
-
-    private static final Path CHECKOUT_PATH = Paths.get("target/checkout/");
 
     @Inject
     LoggerService loggerService;
@@ -42,21 +41,121 @@ public class ReleaseSonatypeSyncTask implements Function<Repository, String> {
         if (!mavenService.isMavenProject(repo)) {
             throw new JobException("NO_POM", "No pom.xml for repo " + repo);
         }
+        Path repoPath = preferences.get(GitService.BASE_PATH_PREFERENCE).resolve(repo.getName());
 
-        Path repoPath = preferences.get(GitService.BASE_PATH_PREFERENCE)
-                .resolve(repo.getName()).resolve(CHECKOUT_PATH);
-        String[] args = {
-                "-B", // non-interactive batch mode
-                "nexus-staging:release"
-        };
+        List<StagingRepo> repos = getStagingRepos(repoPath);
+        if(repos.isEmpty()) {
+            throw new JobException("NO_STAGING_REPO", "Staging repos not found, check release perform stage logs.");
+        }
+        if(repos.size() > 1) {
+            throw new JobException("MULTI_STAGING_REPO",
+                    "Multiple staging repos found, can't automatically deal with them. "
+                    + "Please go to https://oss.sonatype.com and check them manually.");
+        }
 
-        try {
-            desktopService.runMavenCommand(repoPath, args);
-        } catch (DesktopException ex) {
-            throw new JobException(ex.getMessage(), ex);
+        StagingRepo stagingRepo = repos.get(0);
+        if(stagingRepo.state == RepoState.UNKNOWN) {
+            throw new JobException("STAGING_REPO_UNKNOWN_STATE", "Staging repo is in unknown or unsupported state." +
+                    "Please go to https://oss.sonatype.com and check it manually.");
+        }
+
+        if(stagingRepo.state == RepoState.OPEN) {
+            closeStagingRepo(repoPath, stagingRepo);
+        }
+
+        if(stagingRepo.state != RepoState.RELEASED) {
+            releaseStagingRepo(repoPath, stagingRepo);
         }
 
         releaseService.saveRelease(repo);
         return "";
+    }
+
+    private void closeStagingRepo(Path localRepoPath, StagingRepo remoteRepo) {
+        stagingPlugin(localRepoPath, "rc-close", "-DstagingRepositoryId=" + remoteRepo.getId());
+    }
+
+    private void releaseStagingRepo(Path localRepoPath, StagingRepo remoteRepo) {
+        stagingPlugin(localRepoPath, "rc-release", "-DstagingRepositoryId=" + remoteRepo.getId());
+    }
+
+    private List<StagingRepo> getStagingRepos(Path repoPath) {
+        String result = stagingPlugin(repoPath, "rc-list");
+        return result.lines()
+                .dropWhile(s -> !s.startsWith("[INFO] Getting list of available staging repositories..."))
+                .skip(3)
+                .takeWhile(s -> !s.startsWith("[INFO] ------"))
+                .map(s -> s.substring("[INFO] ".length()).trim().split(" "))
+                .map(StagingRepo::of)
+                .collect(Collectors.toList());
+    }
+
+    private String stagingPlugin(Path repoPath, String... commands) {
+        String result;
+        try {
+            commands[0] = "nexus-staging:" + commands[0];
+            result = desktopService.runMavenCommand(repoPath, commands);
+        } catch (DesktopException ex) {
+            throw new JobException(ex.getMessage(), ex);
+        }
+        return result;
+    }
+
+    private enum RepoState {
+        OPEN,
+        CLOSED,
+        RELEASED,
+        UNKNOWN;
+
+        static RepoState of(String value) {
+            try {
+                return RepoState.valueOf(value);
+            } catch (Exception ignore) {
+                return UNKNOWN;
+            }
+        }
+    }
+
+    private static class StagingRepo {
+        private final String id;
+        private final RepoState state;
+        private final String description;
+
+        static StagingRepo of(String... data) {
+            if(data.length < 3) {
+                throw new IllegalArgumentException();
+            }
+            RepoState state = null;
+            StringBuilder description = new StringBuilder();
+            for(int i = 1; i < data.length; i++) {
+                if(state == null) {
+                    if (!data[i].isBlank()) {
+                        state = RepoState.of(data[i]);
+                    }
+                } else {
+                    description.append(data[i]).append(" ");
+                }
+            }
+
+            return new StagingRepo(data[0], state, description.toString());
+        }
+
+        private StagingRepo(String id, RepoState state, String description) {
+            this.id = id;
+            this.state = state;
+            this.description = description;
+        }
+
+        public String getId() {
+            return id;
+        }
+
+        public RepoState getState() {
+            return state;
+        }
+
+        public String getDescription() {
+            return description;
+        }
     }
 }
---
+++ src/test/java/io/bootique/tools/release/service/tasks/ReleaseSonatypeSyncTaskTest.java
@@ -0,0 +1,56 @@
+package io.bootique.tools.release.service.tasks;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import io.bootique.tools.release.model.persistent.Repository;
+import io.bootique.tools.release.service.desktop.DesktopService;
+import io.bootique.tools.release.service.logger.LoggerService;
+import io.bootique.tools.release.service.maven.MavenService;
+import io.bootique.tools.release.service.preferences.Preference;
+import io.bootique.tools.release.service.preferences.PreferenceService;
+import io.bootique.tools.release.service.release.ReleaseService;
+import org.junit.jupiter.api.Test;
+
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+class ReleaseSonatypeSyncTest {
+
+    @Test
+    public void test() {
+        PreferenceService preferenceService = mock(PreferenceService.class);
+        when(preferenceService.get(any(Preference.class))).thenReturn(Paths.get("."));
+
+        MavenService mavenService = mock(MavenService.class);
+        when(mavenService.isMavenProject(any(Repository.class))).thenReturn(true);
+
+        DesktopService desktopService = mock(DesktopService.class);
+        when(desktopService.runMavenCommand(any(Path.class), any(String[].class)))
+                .thenReturn(TEST_MVN_OUTPUT);
+
+        ReleaseSonatypeSyncTask sync = new ReleaseSonatypeSyncTask();
+        sync.preferences = preferenceService;
+        sync.mavenService = mavenService;
+        sync.desktopService = desktopService;
+        sync.loggerService = mock(LoggerService.class);
+        sync.releaseService = mock(ReleaseService.class);
+
+        Repository repository = new Repository();
+        repository.setName("bootique");
+
+        sync.apply(repository);
+    }
+
+    private static final String TEST_MVN_OUTPUT =
+            "[INFO]  * Connected to Nexus at https://oss.sonatype.org:443/, is version 2.14.20-02 and edition \"Professional\"\n" +
+                    "[INFO] Getting list of available staging repositories...\n" +
+                    "[INFO] \n" +
+                    "[INFO] ID                   State    Description                   \n" +
+                    "[INFO] iobootique-1357      OPEN     Implicitly created (auto staging).\n" +
+                    "[INFO] ------------------------------------------------------------------------\n" +
+                    "[INFO] Reactor Summary for bootique-framework-parent 2.0.B2-SNAPSHOT:\n" +
+                    "[INFO] \n" +
+                    "[INFO] bootique-framework-parent .......................... SUCCESS [  4.525 s]\n";
+}
