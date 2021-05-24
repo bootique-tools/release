77b533535be6d2ef13a561ca90848e02b4067ad6
Nikita Timofeev <stariy95@gmail.com>
2021-05-24 15:43:24 +0300

    Fix Maven service on Windows

--- maven.bat
+++ maven.bat
@@ -1,3 +1,2 @@
 SET args=%*
-ECHO "mvn -f %args% -Darguments=\"-Dgpg.pinentry-mode=default -DskipTests\""
-cmd.exe /C mvn -f %args% -Darguments=\"-Dgpg.pinentry-mode=default -DskipTests\"
+cmd.exe /C mvn -f %args% -Darguments="-Dgpg.pinentry-mode=default -DskipTests"
