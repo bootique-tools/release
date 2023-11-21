7471e20c670bacf838c90aeb4da6b6bd0ecd8e1c
stariy95 <stariy95@gmail.com>
2023-11-21 17:36:42 +0400

    Current release process is stable on Java 11 only

--- maven.sh
+++ maven.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-#export JAVA_HOME=`/usr/libexec/java_home -v 1.8`
+#export JAVA_HOME=`/usr/libexec/java_home -v 11`
 args="$*"
 echo "mvn -f $args -Darguments=\"-Dgpg.pinentry-mode=default -DskipTests\""
 mvn -f $args -Darguments="-Dgpg.pinentry-mode=default -DskipTests"
--- release.sh
+++ release.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-#export JAVA_HOME=`/usr/libexec/java_home -v 17`
+#export JAVA_HOME=`/usr/libexec/java_home -v 11`
 echo "mvn -f $1 -B release:$2 $3 -P gpg -DskipTests -Dgpg.pinentry-mode=default -Darguments=\"-Dgpg.pinentry-mode=default -DskipTests\" -Dgoals=\"source:jar-no-fork javadoc:jar deploy\""
 mvn -f $1 -B release:$2 $3 -P gpg -DskipTests -Dgpg.pinentry-mode=default -Darguments="-Dgpg.pinentry-mode=default -DskipTests" -Dgoals="source:jar-no-fork javadoc:jar deploy"
